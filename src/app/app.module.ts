0e141531b07c36bf81251509a808626c069db735
Carlos <carlosmonteroreina@gmail.com>
2018-05-15 16:41:28 +0200

    Created components Main, Login, Poll.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from './login/login.component';
+import {MainComponent} from './main/main.component';
+import {PollComponent} from './poll/poll.component';
 
+
+
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'poll', component: PollComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    PollComponent
   ],
+  
   imports: [
     BrowserModule
   ],
